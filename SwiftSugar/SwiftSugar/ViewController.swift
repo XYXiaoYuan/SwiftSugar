debd069a117e5b98c6db57edec57b5c8448187e7
袁小荣 <664995775@qq.com>
2017-05-13 23:40:16 +0800

    提交代码

---
+++ SwiftSugar/SwiftSugar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftSugar
+//
+//  Created by 袁小荣 on 2017/5/13.
+//  Copyright © 2017年 袁小荣. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
